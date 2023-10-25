637e65c2dd6035cb677228b70fcf979698d5c9be
hablu <131282586+hablu@users.noreply.github.com>
2023-10-25 10:53:11 +0600

    bistroo boss area create and responsive complete

---
+++ src/Pages/Home/BistroBoss/BistroBoss.jsx
@@ -0,0 +1,14 @@
+import './BistroBoss.css';
+
+const BistroBoss = () => {
+  return (
+    <div className='bg-img py-24 bg-fixed'>
+      <div className=' w-2/3 mx-auto p-16 bg-white space-y-3'>
+        <h2 className=' text-4xl uppercase'>Bistro Boss</h2>
+      <p className=' text-[16px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, libero accusamus laborum deserunt ratione dolor officiis praesentium! Deserunt magni aperiam dolor eius dolore at, nihil iusto ducimus incidunt quibusdam nemo.</p>
+      </div>
+    </div>
+  );
+};
+
+export default BistroBoss;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner/Banner';
 import Order from './Order/Order';
+import BistroBoss from './BistroBoss/BistroBoss';
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <Order />
+      <BistroBoss />
     </div>
   );
 };
